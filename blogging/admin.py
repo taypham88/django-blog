24813fc0b175caabadc5270917f3cb7b28beac97
taypham88 <92409737+taypham88@users.noreply.github.com>
2022-05-28 16:48:16 -0700

    try new commit

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
