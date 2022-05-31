b0f7ca2aee6790f304953db1a785c4c5443330b4
taypham88 <92409737+taypham88@users.noreply.github.com>
2022-05-30 19:14:22 -0700

    blogging update

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     exclude = ('posts',)
 
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     inlines = [
         CategoryInline
     ]
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -13,20 +16,31 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+    # published = Post.objects.exclude(published_date__exact=None)
+    # queryset = published.order_by('-published_date')
+    queryset = Post
+    template_name = 'blogging/list.html'
+
+class BlogDetailView(DetailView):
+
+    # published = Post.objects.exclude(published_date__exact=None)
+    # queryset = published.order_by('-published_date')
+    queryset = Post.objects.all()
+    template_name = 'blogging/detail.html'
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
