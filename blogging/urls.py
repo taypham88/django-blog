0b3be94e3ce3844a34aade47893b7f4e34e0292a
taypham88 <92409737+taypham88@users.noreply.github.com>
2022-06-05 16:06:23 -0700

    added form

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import BlogListView, BlogDetailView
+from blogging.views import BlogListView, BlogDetailView, post_create_view
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path('add/', post_create_view),
 ]
